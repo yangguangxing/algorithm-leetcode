58f8f22cc9917104f549ab197c8650266af644ad
yanggx <guangxingyang@126.com>
2021-04-14 14:02:21 +0800

    21

--- src/com/leetcode/no1_100/Solution21_30.java
+++ src/com/leetcode/no1_100/Solution21_30.java
@@ -2,11 +2,14 @@ package com.leetcode.no1_100;
 
 import com.leetcode.ListNode;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Solution21_30 {
 
     /**
      * 21. 合并两个有序链表 https://leetcode-cn.com/problems/merge-two-sorted-lists/
-     * 
+     *
      * @param l1
      * @param l2
      * @return
@@ -15,8 +18,17 @@ public class Solution21_30 {
         // 输入：l1 = [1,2,4], l2 = [1,3,4],输出：[1,1,2,3,4,4]
         ListNode dummy = new ListNode(0), temp = dummy;
         ListNode temp1 = l1, temp2 = l2;
-        while (temp1 != null && temp2 != null) {
-            if (temp1.val < temp2.val) {
+        while (temp1 != null || temp2 != null) {
+            if (temp1 == null) {
+                //如果temp1为null，temp2直接排序
+                temp.next = new ListNode(temp2.val);
+                temp2 = temp2.next;
+            } else if (temp2 == null) {
+                //如果temp2为null，temp1直接排序
+                temp.next = new ListNode(temp1.val);
+                temp1 = temp1.next;
+            } else if (temp1.val < temp2.val) {
+                //两个都不为null
                 temp.next = new ListNode(temp1.val);
                 temp1 = temp1.next;
             } else {
@@ -28,11 +40,25 @@ public class Solution21_30 {
         return dummy.next;
     }
 
+    /**
+     * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+     * 输入：n = 3
+     * 输出：["((()))","(()())","(())()","()(())","()()()"]
+     * 输入：n = 1
+     * 输出：["()"]
+     *
+     * @param n
+     * @return
+     */
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new LinkedList<>();
+        return result;
+    }
+
     public static void main(String[] args) {
         Solution21_30 solution = new Solution21_30();
-        ListNode l1 = ListNode.init(1, 2, 4);
-        ListNode l2 = ListNode.init(1, 3, 4);
-        var result = solution.mergeTwoLists(l1, l2);
+        int n = 3;
+        var result = solution.generateParenthesis(n);
         System.out.println(result);
     }
 }
